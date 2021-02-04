c00b64acb8e75ec7103991a56cf3ef2b849c5013
Vishwas <Vishwas>
2021-02-04 23:20:50 +0000

    intial commit

---
+++ Library/Library/Data/LibraryDataContext.cs
@@ -0,0 +1,27 @@
+﻿using Library.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Data
+{
+    public class LibraryDataContext : DbContext
+    {
+        public DbSet<User> User { get; set; }
+
+        public LibraryDataContext(DbContextOptions<LibraryDataContext> options) : base(options)
+        {
+           
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+       .ToTable("tblBarrower");
+        }
+
+    }
+}
